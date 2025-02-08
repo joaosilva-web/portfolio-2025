fe6a378adee838997be89def7289a856317eda97
joaosilva-web <joaogustavosilva2001@gmail.com>
2025-02-08 17:22:10 -0300

    initial config

---
+++ src/styles/themes/default.ts
@@ -0,0 +1,10 @@
+
+
+export const defaultTheme = {
+    primary: "#3943b7",
+    secondary: "#D1D5EE",
+    hover: "#2F39A0",
+    primaryText: "#1D265E",
+    secondaryText: "#6B728E",
+    backGroundColor: "#F5F7FE",
+}
