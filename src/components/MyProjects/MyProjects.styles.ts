fa7d1a2a6317ff6ac8fa2ea45da2627a1b9a0cb8
João Gustavo <joaogustavosilva2001@gmail.com>
2025-02-26 22:33:00 -0300

    feat: create my projects section

--- src/components/MyProjects/MyProjects.styles.ts
+++ src/components/MyProjects/MyProjects.styles.ts
@@ -1,3 +1,4 @@
+import { motion } from "framer-motion";
 import styled from "styled-components";
 
 
@@ -20,4 +21,41 @@ export const GridContainer = styled.div`
     grid-template-columns: repeat(auto-fill, minmax(400px, 1fr));
     place-items: center;
     gap: 2rem;
+`
+
+export const ProjectCard = styled(motion.div)`
+    position: relative;
+    overflow: hidden;
+    background-color: ${({theme}) => theme.white};
+    border-radius: 16px;
+    width: 400px;
+    height: 250px;
+
+    img {
+        width: 100%
+    }
+`
+
+export const ProjectCardContent = styled(motion.div)`
+top: 0;
+left: 0;
+position: absolute;
+transition: opacity 0.3s ease-in-out;
+opacity: 0;
+display: none;
+background-color: rgba(0, 0, 0, 0.7);
+backdrop-filter: blur(4px);
+border-radius: 16px;
+display: flex;
+flex-direction: column;
+justify-content: center;
+align-items: center;
+text-align: center;
+gap: 0.5rem;
+height: 100%;
+
+h2, p, a {
+    padding: 0;
+    color: ${({theme}) => theme.white};
+}
 `
--- src/components/MyProjects/MyProjects.tsx
+++ src/components/MyProjects/MyProjects.tsx
@@ -1,26 +1,78 @@
-import * as S from "./MyProjects.styles.ts"
+import * as S from "./MyProjects.styles.ts";
 
 interface IProjectInfos {
-    imgSrc: string;
-    projectUrl: string;
-    title: string;
- 
+  imgSrc: string;
+  projectUrl: string;
+  projectName: string;
+  title: string;
 }
 
-const projectsData:IProjectInfos[] = [
-    {imgSrc: "project-bg.png", projectUrl: "#", title: "Results-oriented, responsive and flexible full service civil engineering firm."},
-    {imgSrc: "project-bg.png", projectUrl: "#", title: "Results-oriented, responsive and flexible full service civil engineering firm."},
-    {imgSrc: "project-bg.png", projectUrl: "#", title: "Results-oriented, responsive and flexible full service civil engineering firm."},
-    {imgSrc: "project-bg.png", projectUrl: "#", title: "Results-oriented, responsive and flexible full service civil engineering firm."},
-    {imgSrc: "project-bg.png", projectUrl: "#", title: "Results-oriented, responsive and flexible full service civil engineering firm."},
-    {imgSrc: "project-bg.png", projectUrl: "#", title: "Results-oriented, responsive and flexible full service civil engineering firm."},
-]
+const projectsData: IProjectInfos[] = [
+  {
+    imgSrc: "project-bg.png",
+    projectUrl: "#",
+    projectName: 'Project',
+    title:
+      "Results-oriented, responsive and flexible full service civil engineering firm.",
+  },
+  {
+    imgSrc: "project-bg.png",
+    projectUrl: "#",
+    projectName: 'Project',
+    title:
+      "Results-oriented, responsive and flexible full service civil engineering firm.",
+  },
+  {
+    imgSrc: "project-bg.png",
+    projectUrl: "#",
+    projectName: 'Project',
+    title:
+      "Results-oriented, responsive and flexible full service civil engineering firm.",
+  },
+  {
+    imgSrc: "project-bg.png",
+    projectUrl: "#",
+    projectName: 'Project',
+    title:
+      "Results-oriented, responsive and flexible full service civil engineering firm.",
+  },
+  {
+    imgSrc: "project-bg.png",
+    projectUrl: "#",
+    projectName: 'Project',
+    title:
+      "Results-oriented, responsive and flexible full service civil engineering firm.",
+  },
+  {
+    imgSrc: "project-bg.png",
+    projectUrl: "#",
+    projectName: 'Project',
+    title:
+      "Results-oriented, responsive and flexible full service civil engineering firm.",
+  },
+];
 
 export function MyProjects() {
-    return(
-        <S.MyProjectsContainer>
-            <h2>My Projects</h2>
-            
-        </S.MyProjectsContainer>
-    )
+  return (
+    <S.MyProjectsContainer>
+      <h2>My Projects</h2>
+      <S.GridContainer>
+        {projectsData.map((project) => (
+          <S.ProjectCard
+            // whileHover={{ scale: 1.05 }}
+          >
+            <img src={project.imgSrc} alt={project.title} />
+            <S.ProjectCardContent
+              whileHover={{ opacity: 1, display: "flex" }}
+              transition={{ ease: "easeIn", duration: 0.5  }}
+            >
+                <h2>{project.projectName}</h2>
+              <p>{project.title}</p>
+              <a href={project.projectUrl}>Visit Project</a>
+            </S.ProjectCardContent>
+          </S.ProjectCard>
+        ))}
+      </S.GridContainer>
+    </S.MyProjectsContainer>
+  );
 }
